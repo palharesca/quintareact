8bda4f2bafc5cc280b48e9554d718e282348c7ed
CAMILLA LELIS PALHARES <camilla.lpalhares@senacsp.edu.br>
2022-03-24 22:08:19 -0300

    Criar funções no component e executá-las interpolando com chaves

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,26 @@ import './App.css';
 import Header from './Header';
 
 function App() {
+  
+  function handleButtonClick() {
+    alert("Clicou no botão");
+  }
+
+  function sum(a,b){
+    return a+b
+
+  }
+
+
   return (
     <>
         <Header title="Meu primeiro Component React" />
         <Header title="Um Component com props" />
         <h2>
           Senac Campinas
+          <div>Retorno da Soma:{sum(5,6)}</div>
         </h2>
+        <button onClick={handleButtonClick}>Click Me!</button>
        
     </>
   );
